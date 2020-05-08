009c63a25f9879d49647c9b69bb5b811a673e1d4
Welly T Gee <wellytg@gmail.com>
2020-05-08 02:19:04 +0200

    change port

--- script.js
+++ script.js
@@ -105,5 +105,5 @@ function validateEquip(equip) {
 }
 
 //PORT ENVIRONMENT VARIABLE
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}..`));
